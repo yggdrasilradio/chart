8025cbba72bb6417a097424d8dcc7736a2c9cbcb
Rick Adams <webmaster@rickadams.org>
2022-01-04 18:33:04 -0600

    Initial commit

---
+++ Makefile
@@ -0,0 +1,9 @@
+all: chart
+
+chart: chart.bas
+	lwasm -9 -f basic -o /tmp/data.bas chart.asm
+	fgrep DATA /tmp/data.bas | sed -e 's/,-1,-1//' -e 's/^[^ ]*//' > chart.dat
+	decbpp < chart.bas > /tmp/chart.bas
+	decb copy -tr /tmp/chart.bas /media/share1/COCO/drive0.dsk,CHART.BAS
+	cat /tmp/chart.bas
+	rm -f /tmp/chart.bas chart.dat data.bas
---
+++ chart.asm
@@ -0,0 +1,55 @@
+
+       org     $1000
+
+       orcc    #$50     * disable interrupts
+       ldb     #$3e	* VSYNC use rising edge
+       stb     $ff03
+       ldb     #$35	* enable HSYNC IRQ
+       stb     $ff01
+       clr     $ff02    * init keyboard
+
+loop   lda     #16	* going to change palettes 16 times
+       ldx     #$0010	* initial palette values for columns 1 and 2
+       ldu     #$2030	* initial palette values for columns 3 and 4
+       ldb     #70	* initially wait for 70 HSYNCs before changing palettes
+
+       * wait for VSYNC
+       tst     $ff02	* dismiss VSYNC
+vwait  tst     $ff03
+       bpl     vwait
+
+       * wait for 70 HSYNCs 1x, then wait for 8 HSYNCs 16x
+hwait  tst     $ff00	* dismiss HSYNC
+       sync
+
+       decb		* count HSYNCs
+       bne     hwait	* keep going
+
+       * wait for HSYNC before changing palettes
+       tst     $ff00	* dismiss HSYNC
+       sync
+
+       stx     $ffb8	* change palettes for columns 1 and 2
+       stu     $ffba	* change palettes for columns 3 and 4
+       leax    $0101,x	* increment palette values for columns 1 and 2
+       leau    $0101,u	* increment palette values for columns 3 and 4
+
+       ldb     #7	* done waiting for 70 HSYNCs, waiting for 8 from here on out
+
+       deca		* count down from 16
+       bne     hwait	* next series of HSYNCs
+
+       * Is a key down?
+       pshs d
+       lda $ff00
+       coma
+       lsla
+       bne keydn	* key is down
+       puls d
+       bra loop		* no key down, keep going
+
+       * Hard reset to RSDOS
+keydn  clr     $71	* force hard reset
+       jmp     [$fffe]	* jump thru reset vector
+
+
